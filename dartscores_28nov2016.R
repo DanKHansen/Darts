e954ff8f4e40a5f906f1fdadc7cfe165519371ab
DanKHansen <dankh@live.dk>
2017-02-01 21:49:06 +0100

    stuff

--- dartscores_28nov2016.R
+++ dartscores_28nov2016.R
@@ -4,7 +4,8 @@ scores1 <- c(26,33,85,60,41,35,7,26,52,45,53,45,26,43,41,55,30,40,41,121,39,43,3
 scores2 <- c(41,30,33,25,100,23,40,9,50,100,43,45,60,49,85,83,58,17,45,65,50,30,24,52,86,58,28,41,52,100,45,30,45,73,73,25,100,14,50,26,85,45,26,44,21,15,39,45,43,26)
 #Scores3 23JAN17
 scores3 <- c(85,40,70,9,58,45,26,45,85,7,23,30,22,41,45,28,26,100,95,100,41,45,26,26,81,30,31,180,85,41,60,66,100,26,81,16,33,70,24,30,45,45,140,81,30,41,60,17,60,45)
-
+#Scores3 01FEB17
+scores4 <- c(97,36,26,30,28,11,39,26,22,45,86,30,41,37,9,85,85,39,53,70,95,33,26,11,26,34,20,49,41,94,45,60,41,22,60,30,43,45,36,95,39,26,95,7,28,66,26,7,80,11)
 
 plot(scores3,col="blue",pch=19,type = 'b')
 abline(h=mean(scores3),col='blue')
@@ -12,8 +13,10 @@ points(scores2,col="red",pch='2',type = 'p')
 abline(h=mean(scores2), col="red")
 points(scores1,col="green",pch='1',type = 'p')
 abline(h=mean(scores1), col="green")
-c(max(scores1),max(scores2),max(scores3))
-c(min(scores1),min(scores2),min(scores3))
-c(mean(scores1),mean(scores2),mean(scores3))
-c(median(scores1),median(scores2),median(scores3))
+points(scores4,col="black",pch='4',type = 'p')
+abline(h=mean(scores1), col="yellow")
+c(max(scores1),max(scores2),max(scores3),max(scores4))
+c(min(scores1),min(scores2),min(scores3),min(scores4))
+c(mean(scores1),mean(scores2),mean(scores3),mean(scores4))
+c(median(scores1),median(scores2),median(scores3),median(scores4))
 
